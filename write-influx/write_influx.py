b2a98221fc43872a00fabe2f61508976c68eee51
Blaise Thompson <blaise@untzag.com>
2020-10-30 13:27:06 -0500

    local broker

--- write-influx/write_influx.py
+++ write-influx/write_influx.py
@@ -86,7 +86,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("mosquitto.chem.wisc.edu", 1883, 60)
+client.connect("mqtt", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
